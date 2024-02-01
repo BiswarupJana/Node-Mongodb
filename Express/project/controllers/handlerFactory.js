42f019c1821e3ad4266d2cf5e3db84d77cb3fc21
Biswarup Jana <biswaruprx21@gmail.com>
2024-02-01 22:25:31 +0530

    #

--- Express/project/controllers/handlerFactory.js
+++ Express/project/controllers/handlerFactory.js
@@ -78,7 +78,7 @@ exports.getAll = Model =>
       .limitFields()
       .paginate();
     // const doc = await features.query.explain();
-    const doc = await features.query;
+    const doc = await features.query.explain();
 
     // SEND RESPONSE
     res.status(200).json({
--- Express/project/models/tourModel.js
+++ Express/project/models/tourModel.js
@@ -116,6 +116,9 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
